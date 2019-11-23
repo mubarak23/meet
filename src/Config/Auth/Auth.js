31a4e5da4587bbb133a4c4a9a2992885ca3c2a8f
mubarak23 <mubarakaminu340@gmail.com>
2019-11-23 08:12:05 +0100

    ft(home):create the home component and link the nav bar[progress]

---
+++ src/Config/Auth/Auth.js
@@ -0,0 +1,9 @@
+import { EventEmitter } from 'events';
+
+export default class AuthService extends EventEmitter {
+    // eslint-disable-next-line no-useless-constructor
+    constructor(){
+        super();
+    }
+    
+} 
