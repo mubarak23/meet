4004598d51270dbec807b90063fb146f4a0bc2d0
mubarak23 <mubarakaminu340@gmail.com>
2019-11-23 09:13:09 +0100

    ft(home):create login and signup component link with router[finish]

--- src/Component/Home.js
+++ src/Component/Home.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 class Home extends Component {
@@ -6,8 +7,14 @@ class Home extends Component {
     render() {
         return (
            <div className="container">
-               <div>
-               
+               <div className="jumbotron">
+               <h1 className="display-4">Meet</h1>
+                <p className="lead">Platform that allow you organize all your 
+                shecdule meeting</p>
+                <p>Your can create meeting for you team member and can also 
+                    sigup or unregister for a meetup</p>
+                <Link className="btn btn-primary btn-lg" to="signup"
+                 role="button">Signup</Link>
                </div>
            </div> 
         );
--- src/Component/Nav.js
+++ src/Component/Nav.js
@@ -5,19 +5,16 @@ class Nav extends Component {
     render() {
         return (
             <div>
-             <nav class="navbar navbar-expand-lg navbar-light bg-light">
+             <nav class="navbar navbar-expand-lg navbar-primary bg-light">
             <Link className="navbar-brand" to="/home">Meeting</Link>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
             <div className="collapse navbar-collapse" id="navbarText">
                 <ul className="navbar-nav mr-auto">
-                <li className="nav-item active">
-                    <Link className="nav-link" href="#">Home 
-                    <span class="sr-only">(current)</span></Link>
-                </li>
+                
                 <li className="nav-item">
-                    <Link className="nav-link" to="/singup">Signup</Link>
+                    <Link className="nav-link" to="/signup">Signup</Link>
                 </li>
                 <li class="nav-item">
                     <Link className="nav-link" to="/login">Login</Link>
