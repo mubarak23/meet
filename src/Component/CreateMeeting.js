938d7ef3b11d059da91cb8750aa5a76560fb2b3e
mubarak23 <mubarakaminu340@gmail.com>
2019-11-30 13:28:23 +0100

    ft(create): form field for creating meeting[progress]

--- src/Component/CreateMeeting.js
+++ src/Component/CreateMeeting.js
@@ -1,14 +1,40 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class CreatMeeting extends Component {
-
-    render() {
-        return (
+  render() {
+    return (
+      <div className="container">
+        <div className="col-10 offset-2">
+          <h3 className="lead">Create Meeting</h3>
+          <form className="form-group">
             <div>
-                
+              <label htmlFor="Title">Title</label>
+              <input
+                type="text"
+                name="title"
+                required
+                placeholder="Meeting Title"
+              />
             </div>
-        );
-    }
+            <div>
+              <label htmlFor="description">Description</label>
+              <input
+                type="text"
+                name="description"
+                required
+                placeholder="Meeing Description"
+              />
+            </div>
+            <input
+              type="submit"
+              className="btn btn-primary"
+              value="Creating Meeting"
+            />
+          </form>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default CreatMeeting;
