2d9650b160bc10d885bdcf0ded22f2e5d5a217df
mubarak23 <mubarakaminu340@gmail.com>
2020-06-12 16:19:46 +0100

    connect signindata to api

--- src/Component/Login.js
+++ src/Component/Login.js
@@ -1,20 +1,53 @@
 import React, { Component, useState } from 'react';
 import axios from 'axios';
-import { withRouter, Link, useHistory } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 //import  auth  from '../Config/Auth/Auth';
 
 const Signin = () => {
   const history = useHistory();
   const [email, SetEmail] = useState('');
   const [password, SetPassword] = useState('');
-  const signindata = () => {};
+  const signindata = () => {
+    fetch('/http://127.0.0.1:8000/api/v1/user/signin', {
+      method: 'post',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        email,
+        password,
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log('data');
+        if (data.error) {
+          return (
+            <div className='toast'>
+              <div className='toast-body'>{data.error}</div>
+            </div>
+          );
+        } else {
+          history.push('/meeting');
+          localStorage.setItem('token', data.Token);
+          return (
+            <div className='toast'>
+              <div className='toast-body'>{data.message}</div>
+            </div>
+          );
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
   return (
     <div className='container'>
       <div className='col-md-4 col-md-offset-4'>
         <div className='row'>
           <h2>Login</h2>
         </div>
-        <form className='form-group' onSubmit={this.onLoginSubmit}>
+        <form className='form-group'>
           <div>
             <label>Email</label>
             <input
@@ -37,86 +70,20 @@ const Signin = () => {
           </div>
           <div>
             <br></br>
-            <input type='submit' className='btn btn-primary' value='Login' />
+            <input
+              type='submit'
+              onClick={() => signindata()}
+              className='btn btn-primary'
+              value='Login'
+            />
           </div>
         </form>
+        <h5>
+          <Link to='/login'>Yet Signup</Link>
+        </h5>
       </div>
     </div>
   );
 };
-
-class Login extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      email: '',
-      password: '',
-      loginerror: '',
-    };
-    this.onFieldChange = this.onFieldChange.bind(this);
-    this.onLoginSubmit = this.onLoginSubmit.bind(this);
-  }
-  onFieldChange(event) {
-    this.setState({
-      [event.target.name]: event.target.value,
-    });
-  }
-
-  onLoginSubmit(event) {
-    event.preventDefault();
-    console.log(this.state.email);
-    axios
-      .post('http://127.0.0.1:8000/api/v1/user/signin', {
-        email: this.state.email,
-        password: this.state.password,
-      })
-      .then((response) => {
-        const correct_res = JSON.stringify(response);
-        console.log(correct_res);
-
-        console.log(response.data.Token);
-        //push data to local storage
-
-        localStorage.setItem('token', response.data.Token);
-        this.props.history.push('/meeting');
-      });
-  }
-
-  render() {
-    return (
-      <div className='container'>
-        <div className='col-md-4 col-md-offset-4'>
-          <div className='row'>
-            <h2>Login</h2>
-          </div>
-          <form className='form-group' onSubmit={this.onLoginSubmit}>
-            <div>
-              <label>Email</label>
-              <input
-                type='text'
-                onChange={this.onFieldChange}
-                className='form-control'
-                name='email'
-              />
-            </div>
-            <div>
-              <label>Password</label>
-              <input
-                type='password'
-                className='form-control'
-                onChange={this.onFieldChange}
-                name='password'
-              />
-            </div>
-            <div>
-              <br></br>
-              <input type='submit' className='btn btn-primary' value='Login' />
-            </div>
-          </form>
-        </div>
-      </div>
-    );
-  }
-}
 
 export default Signin;
