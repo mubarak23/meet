47f2d75989b2a920deeb9e3e8297561bc92db6e7
mubarak23 <mubarakaminu340@gmail.com>
2020-06-14 12:32:20 +0100

    UI for meeting creation

--- src/Component/CreateMeeting.js
+++ src/Component/CreateMeeting.js
@@ -1,82 +1,65 @@
-import React, { Component } from "react";
+import React, { Component, useState, useContext, useEffect } from 'react';
+import { Link, useHistory } from 'react-router-dom';
+import { userContext } from '../App';
 
-class CreatMeeting extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      title: "",
-      description: "",
-      date: ""
-    };
-    this.onCreateSubmit = this.onCreateSubmit.bind(this);
-    this.onFieldChange = this.onFieldChange.bind(this);
-  }
-  onFieldChange(event) {
-    event.preventDefault();
-    this.setState({
-      [event.target.name]: event.target.value
-    });
-  }
-  onCreateSubmit(event) {
-    event.preventDefault();
-    const body = {
-      title: this.state.title,
-      description: this.state.description,
-      date: this.state.date
-    };
-    console.log(body);
-  }
-  render() {
-    //console.log("Checking Test");
-    return (
-      <div className="container">
-        <div className="col-10 offset-2">
-          <h3 className="lead">Create Meeting</h3>
-          <form className="form-group" onSubmit={this.onCreateSubmit}>
-            <div>
-              <label htmlFor="Title">Title</label>
-              <input
-                type="text"
-                name="title"
-                className="form-control"
-                required
-                onChange={this.onFieldChange}
-                placeholder="Meeting Title"
-              />
-            </div>
-            <div>
-              <label htmlFor="description">Description</label>
-              <input
-                type="text"
-                className="form-control"
-                name="description"
-                required
-                onChange={this.onFieldChange}
-                placeholder="Meeing Description"
-              />
-            </div>
-            <div>
-              <label htmlFor="date">Date</label>
-              <input
-                type="text"
-                name="date"
-                className="form-control"
-                onChange={this.onFieldChange}
-                required
-                placeholder="Meeing Description"
-              />
-            </div>
-            <br></br>
-            <input
-              type="submit"
-              className="btn btn-primary"
-              value="Creating Meeting"
-            />
-          </form>
+const CreatMeeting = () => {
+  const history = useHistory();
+  const [title, Settitle] = useState('');
+  const [description, SetDescription] = useState('');
+  const [date, Setdate] = useState('');
+  const { dispatch } = useContext(userContext);
+  useEffect(() => {
+    const user = JSON.parse(localStorage.getItem('token'));
+    console.log(user);
+    if (user) {
+    } else {
+      history.push('/login');
+    }
+  }, []);
+  return (
+    <div className='container'>
+      <div className='col-10 offset-2'>
+        <h3 className='lead'>Create Meeting</h3>
+
+        <div>
+          <label htmlFor='Title'>Title</label>
+          <input
+            type='text'
+            value={title}
+            className='form-control'
+            onChange={(e) => Settitle(e.target.value)}
+            placeholder='Meeting Title'
+          />
         </div>
+        <div>
+          <label htmlFor='description'>Description</label>
+          <input
+            type='text'
+            className='form-control'
+            name={description}
+            onChange={(e) => SetDescription(e.target.value)}
+            placeholder='Meeing Description'
+          />
+        </div>
+        <div>
+          <label htmlFor='date'>Date</label>
+          <input
+            type='date'
+            name={date}
+            className='form-control'
+            onChange={(e) => Setdate(e.target.value)}
+            placeholder='Meeing Description'
+          />
+        </div>
+        <br></br>
+        <input
+          type='submit'
+          className='btn btn-primary'
+          value='Creating Meeting'
+        />
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default CreatMeeting;
